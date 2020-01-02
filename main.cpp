92e1cde89760e67a61f1ccac40d7212a5d05acfd
AleksandraRuzic <ruzic.aleksandra@gmail.com>
2020-01-02 23:16:48 +0100

    Fixed bugs/Finished LinearRegressionNode

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include "SourceNode.hpp"
 #include "Stream.hpp"
 #include "NormalizationNode.hpp"
-#include "StatisticsNode.hpp"
 
 #include <iostream>
 
@@ -19,52 +18,24 @@ int main(int argc, char *argv[])
 
 
     SourceNode sn("SN!");
-<<<<<<< HEAD
     //sn.setHasClass(true);
-    sn.setFilename("../RS012-data-mining-toolbox/iris.csv");
+    sn.setFilename("../RS012-data-mining-toolbox/dogs.csv");
     sn.run();
+    sn.filterBinarisedCol("breed");
 
-    arma::mat matrix = sn.filterBinarisedCol("Species");
-    for(unsigned i = 0; i < matrix.n_rows; i++) {
-        for(unsigned j = 0; j< matrix.n_cols; j++) {
-            std::cout << matrix(i,j) << " ";
-        }
-        std::cout << std::endl;
-    }
-
-   /* for(auto col : sn.OutputDataTable().ColumnNames())
-    {
-        std::cout << col << " ";
-    }
-    std::cout << std::endl;
-
-    for(auto col : sn.OutputDataTable().ClassTargetVariable())
-    {
-        std::cout << col << " ";
-    }
-    std::cout << std::endl;*/
+    LinearRegressionNode lr("LR!");
 
 
-    /*DecisionTreeNode nn("nn");
+    //DecisionTreeNode nn("nn");
 
     Stream s;
     s.add(&sn);
-    s.add(&nn);
-    s.connect_to(&sn, &nn);
-    s.RunStream(&nn);*/
+    s.add(&lr);
+    s.connect_to(&sn, &lr);
+    lr.run();
+    //s.RunStream(&lr);
 
-   // std::cout << sn.OutputDataTable().DataMatrix() << std::endl << std::endl << std::endl << nn.OutputDataTable().DataMatrix() << std::endl;
-=======
-    sn.setFilename("../RS012-data-mining-toolbox/iris.csv");
-
-    StatisticsNode st("st");
-
-    Stream s;
-    s.add(&sn);
-    s.add(&st);
-    s.connect_to(&sn, &st);
-    s.RunStream(&st);
->>>>>>> f57bfa3c82f7ee28866da04c91b69dac3eac0446
+    std::cout << sn.OutputDataTable().DataMatrix() << std::endl << std::endl << std::endl << lr.OutputDataTable().DataMatrix() << std::endl;
 
    // return a.exec();
     return 0;
